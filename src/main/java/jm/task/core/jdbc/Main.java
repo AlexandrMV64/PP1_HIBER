92fa2dee63005c3a75768202ad1d557f9b37fb43
myasnikov <myasnikov64mv@yandex.ru>
2023-06-07 16:01:48 +0400

    DONE

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("Oleg","Ibragimov",(byte) 21);
+        us.saveUser("Jack","Ibrav",(byte) 23);
+        us.saveUser("Sany","gimov",(byte) 25);
+        us.saveUser("Ivan","ragimov",(byte) 41);
+        us.getAllUsers().forEach(System.out::println);
+        us.cleanUsersTable();
+        us.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,40 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static Connection dbConnection;
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String URL = "jdbc:postgresql://localhost:5432/";
+    private static final String DATABASE = "postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "root";
+
+    public static Connection getJDBSConnection(){
+        while (dbConnection == null) {
+
+            try {
+                Class.forName(DRIVER);
+                System.out.println("Драйвер успешно загружен");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Не удалось загрузить драйвер");
+                break;
+            }
+            try {
+                dbConnection = DriverManager.getConnection(
+                        URL + DATABASE, USERNAME, PASSWORD);
+                System.out.println("Успешное подлючение к БД");
+            } catch (SQLException e) {
+                System.out.println("Не удалось подключиться к БД");
+                break;
+            }
+        }
+        return dbConnection;
+    }
+
+
+
     // реализуйте настройку соеденения с БД
 }
